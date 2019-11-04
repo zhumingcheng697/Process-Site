117583021355023912bc678f6b98bbc74fc6149c
McCoy Appleseed <31050761+zhumingcheng697@users.noreply.github.com>
2019-11-04 17:58:05 -0500

    Update ContentView.swift

--- Creative Output/Day 8/ContentView.swift
+++ Creative Output/Day 8/ContentView.swift
@@ -34,31 +34,37 @@ var courses = [
     Course(glyph: "camera", name: "Still and Moving Images", code: "DM-UY 2263 Section D", date: "Tuesday & Thursday", time: "4:30PM - 6:20PM", location: "370 Jay Street, Room 309")
 ]
 
+struct CourseView: View {
+    var course: Course
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: course.glyph)
+                    .font(.largeTitle)
+                    .padding(.trailing, 6)
+                VStack(alignment: .leading) {
+                    Text(course.name)
+                        .font(.title)
+                    Text(course.code)
+                        .font(.subheadline)
+                }
+            }
+            .padding(.vertical,10)
+            
+            Text(course.date)
+            Text(course.time)
+            Text(course.location)
+        }
+        .padding(.vertical)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack{
-            ScrollView{
-                ForEach(courses, id: \.name){ course in
-                    VStack {
-                        HStack {
-                            Image(systemName: course.glyph)
-                                .font(.largeTitle)
-                                .padding(.trailing, 6)
-                            VStack(alignment: .leading) {
-                                Text(course.name)
-                                    .font(.title)
-                                Text(course.code)
-                                    .font(.subheadline)
-                            }
-                        }
-                        .padding(.vertical,10)
-                        
-                        Text(course.date)
-                        Text(course.time)
-                        Text(course.location)
-                    }
-                    .padding(.vertical)
-                }
+        ScrollView{
+            ForEach(courses, id: \.name){ course in
+                CourseView(course: course)
             }
         }
     }
